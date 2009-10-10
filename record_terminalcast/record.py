a80401d650b82bd288cb09e09afab51b88e43b1e
Patrick Mullen <patrickmullen@Macintosh-3.local>
2009-10-09 22:44:04 -0400

    more cleanup

--- record_terminalcast/record.py
+++ record_terminalcast/record.py
@@ -6,16 +6,33 @@ import posixpath
 import signal
 import subprocess
 from zipfile import ZipFile
-
 import optfunc
 from  upload import upload_terminalcast
-
 
 TC_DIR = "~/.terminalcast/"
 LOCAL_TC_HOST = 'localhost:8000'
 #TC_HOST = 'lispnyc.org:8003'
 #TC_HOST = 'terminalcast.paddymullen.com'
 TC_HOST = 'terminalcast.com'
+
+class JackRecord(object):
+
+    def __init__(self,tcast_sound):
+        args = ("/usr/local/bin/ecasound  -i jack,system -o %s" % tcast_sound).split(" ")
+        self.sub_proc = subprocess.Popen(args)
+    def finish(self):
+        self.sub_proc.send_signal(signal.SIGTERM)        
+
+    
+class Afrecord(object):
+
+    def __init__(self, tcast_sound):
+        args = ("/usr/bin/sound_recorders/afrecord -d LEI16 -f WAVE %s" % tcast_sound).split(" ")
+        self.sub_proc = subprocess.Popen(args)
+    def finish(self):
+        self.sub_proc.send_signal(signal.SIGTERM)        
+
+SOUND_RECORDING_CLASS = Afrecord
 
 def get_empty_directory():
     terminalcast_dir = os.path.expanduser(TC_DIR)
@@ -25,7 +42,6 @@ def get_empty_directory():
         pass
     for i in range(1,1000):
         terminalcast_dir = os.path.expanduser("%s%d" % (TC_DIR, i))
-        
         if posixpath.exists(terminalcast_dir):
             continue
         else:
@@ -39,56 +55,16 @@ def ls():
             tcast_desc = "%s/desc.pckl" % terminalcast_dir
             try:
                 description_dict = cPickle.load(open(tcast_desc))
-                print i
-                print description_dict
-                print "-"*80
-
+                print i,description_dict, "\n", "-"*80
             except EOFError, AttributeError:
                 print "error"
 
-class SoundRecorders(object):
-    @staticmethod
-    def jack(tcast_sound):
-        cmd="/usr/local/bin/ecasound"
-        args="ecasound -i jack,system -o %s" % tcast_sound
-        os.execl(cmd, *args.split(" "))
-
-    @staticmethod
-    def mac_afrecord(tcast_sound):
-        cmd="/usr/bin/sound_recorders/afrecord"
-        cmd="/Users/patrickmullen/TerminalcastRecord/record_terminalcast/sound_recorders/mac_afrecord/AudioFileTools/build/Debug-Tiger+/afrecord"
-        args="afrecord -d LEI16 -f WAVE %s" % tcast_sound
-        #os.execl (cmd, *args.split(" "))
-        #        /usr/bin/sound_recorders/afrecord -d LEI16 -f WAVE %s" 
-        os.system('bash -c "/Users/patrickmullen/TerminalcastRecord/record_terminalcast/sound_recorders/mac_afrecord/AudioFileTools/build/Debug-Tiger+/afrecord -d LEI16 -f WAVE %s"' % tcast_sound)
-
-class Afrecord(object):
-
-    def __init__(self, tcast_sound):
-        args = ("/usr/bin/sound_recorders/afrecord -d LEI16 -f WAVE %s" % tcast_sound).split(" ")
-        self.sub_proc = subprocess.Popen(args)
-    def finish(self):
-        self.sub_proc.send_signal(signal.SIGTERM)        
-        
-        
-SOUND_RECORDING_FUNC = SoundRecorders.mac_afrecord
-def TCAST_RECORDINGFUNC(tcast_file, tcast_timing):
+def TCAST_RECORDING_FUNC(tcast_file, tcast_timing):
     os.system("/usr/bin/ttyrec %s %s" % (tcast_file, tcast_timing))
 
 def post_process_sound(tcast_sound, tcast_mp3, tcast_ogg):
     os.system("lame -V 9 %s %s " % (tcast_sound, tcast_mp3))
     os.system("oggenc -q2 --resample 10000 %s -o  %s" % (tcast_sound, tcast_ogg))
-
-def dummy_rec():
-    terminalcast_dir = "/Users/patrickmullen/TerminalcastRecord/dummy"
-    tcast_file = "%s/tcast_data" % terminalcast_dir
-    tcast_timing = "%s/timing.js" % terminalcast_dir
-    tcast_sound = "%s/sound.wav" % terminalcast_dir
-    tcast_mp3 = "%s/sound.mp3" % terminalcast_dir
-    tcast_ogg = "%s/sound.ogg" % terminalcast_dir
-    tcast_zip = "%s/tc.zip" % terminalcast_dir
-    tcast_desc = "%s/desc.pckl" % terminalcast_dir
-
 
 def record(
     username='',    password='',    title='',
@@ -98,13 +74,7 @@ def record(
         if required == '':
             print "missing option  try -h to list options "
             return
-              
-    # Okay, let's do this the hard way: create two unnamed
-    # pipes for stdout/err. Then, fork this process, and the
-    # child becomes the new command and shoves their output
-    # onto the pipes, while the parent waits for the child to die.
-              
-    # Create two named pipes
+
     terminalcast_dir = get_empty_directory()
     tcast_file = "%s/tcast_data" % terminalcast_dir
     tcast_timing = "%s/timing.js" % terminalcast_dir
@@ -113,13 +83,14 @@ def record(
     tcast_ogg = "%s/sound.ogg" % terminalcast_dir
     tcast_zip = "%s/tc.zip" % terminalcast_dir
     tcast_desc = "%s/desc.pckl" % terminalcast_dir
+
     description_dict = dict(
         title=title, description=description, tag_list=tag_list)
     cPickle.dump(description_dict, open(tcast_desc,"w"))
 
-
-    TCAST_RECORDINGFUNC(tcast_file, tcast_timing)
-
+    sound_recorder = SOUND_RECORDING_CLASS()
+    TCAST_RECORDING_FUNC(tcast_file, tcast_timing)
+    sound_recorder.finish()
     post_process_sound(tcast_file, tcast_timing)
 
     zf = ZipFile( tcast_zip ,'w')
@@ -144,12 +115,6 @@ def upload(number='', username='', password='', host=TC_HOST):
     description_dict = cPickle.load(open(tcast_desc))
     upload_terminalcast(tcast_zip, description_dict, username, password, host=host)
 
-def my_main():
-    optfunc.main([upload,ls,record, dummy_rec])
 
 if __name__ == '__main__':
-    my_main()
-    
-    #optfunc.main([upload_saved_terminalcast,record])
-    #optfunc.main([upload,ls,record])
-    #optfunc.main(record)
+    optfunc.main([upload,ls,record)
